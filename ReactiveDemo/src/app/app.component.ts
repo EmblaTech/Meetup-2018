d3a302dfe02ed5278c958cd74e4f953105881f08
Mahesh Livera <MaheshL@embla.lk>
2018-07-30 12:40:18 +0530

    Initial project structure added

---
+++ ReactiveDemo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<div class="container"><h1>Reactive Form Demo</h1></div>',
+  styleUrls: []
+})
+export class AppComponent {
+  title = 'app';
+}
